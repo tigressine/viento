3120307ac65a6119ab7722d3cee81e228a0012fd
Tiger Sachse <tiger@sachse.io>
2017-07-13 16:52:24 -0400

    Alpha release.
    Has not been well tested and still probably contains many bugs. Working diligently to get to beta.

---
+++ acdaemon.py
@@ -0,0 +1,217 @@
+#! /usr/bin/env python3
+"""
+This script is the central daemon behind AutoClone. It pulls custom links from a .json
+file, then it executes rclone commands based on information in the custom links. The
+script outputs to log files defined by the variables f_log1 and f_log2.
+
+Author: tgsachse (Tiger Sachse)
+Initial Release: 7/13/2017
+Current Release: 7/13/2017
+Version: 0.01
+License: GNU GPLv3
+"""
+import os
+import re
+import json
+import time
+import signal
+import threading
+
+### CLASSES ###
+class Link():
+    """
+    Returns Link object.
+    Attributes: f_job, src, dest, interval, method, flags, count, intv_persist, command
+    Functions: __init__, transfer, react, learn
+    """
+    def __init__(self, attributes):
+        """
+        Initializes Link object with several attributes.
+        """
+        self.f_job = f_job_template.format(attributes[0])
+        self.src = attributes[1]
+        self.dest = attributes[2]
+        self.interval = int(attributes[3])
+        self.method = attributes[4]
+        # Not yet implemented.
+        self.flags = ['-r']#attributes[5]
+        
+        self.count = 0
+        self.intv_persist = self.interval
+        self.command = 'rclone -v {0} {1} {2} &> {3}'.format(self.method,
+                                                             self.src,
+                                                             self.dest,
+                                                             self.f_job)
+    def transfer(self):
+        """
+        Executes a system command using self.command.
+        If enabled, link object can react and learn from the results of this command.
+        """
+        os.system(self.command)
+        log('TRANSFER: {0} \'{1}\' >> \'{2}\''.format(self.method,
+                                                      self.src,
+                                                      self.dest))
+        if '-r' in self.flags:
+            self.react()
+        if '-l' in self.flags:
+            self.learn()
+
+    def react(self):
+        """
+        If enabled, the script will react to the outcome of the command executed in
+        transfer(). It does this by using the re module to scan a job.dat file (which
+        contains the verbose output of the command executed in transfer()). If the re
+        module finds a match in the .dat file (e.g. the string ':Copied (new)') then
+        the script will put itself into a heightened mode. This heightened mode results
+        in a shortened interval for transfers, starting at 1 minute intervals and, after
+        enough iterations of transfer() without any more activity, the intervals decay
+        back to original values (stored in self.intv_persist).
+        """
+        job_matches = [': Copied \(new\)',
+                       ': Copied\(replaced existing\)',
+                       ': Deleted']
+        job_match = '|'.join(job_matches)
+        # This list pair defines how long the script will remain heightened, in intervals.
+        react_len = [2,3]
+
+        with open(self.f_job, 'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                if re.search(job_match, line):
+                    self.interval = 1
+                    self.count = 1
+                    log('STATE: entering heightened state for \'{0}\' {1}'.format(self.src,
+                                                                                  self.method))
+                    log('STATE: {0} interval changed to every {1} minute(s)'.format(self.method,
+                                                                                    self.interval))
+                    break
+        
+        if self.count >= react_len[1]:
+            self.interval = self.intv_persist
+            self.count = 0
+            log('STATE: leaving heightened state for \'{0}\' {1}'.format(self.src,
+                                                                         self.method))
+            log('STATE: {0} interval reverted to every {1} minute(s)'.format(self.method,
+                                                                             self.interval))
+        elif self.count >= react_len[0] and self.intv_persist != 1:
+            self.interval = 2
+            self.count += 1
+            log('STATE: {0} interval changed to every {1} minute(s)'.format(self.method,
+                                                                            self.interval))
+        elif self.count > 0:
+            self.count += 1
+
+    def learn(self):
+        """
+        WIP
+        """
+        pass
+
+
+### FUNCTIONS ###
+def check_dirs():
+    """
+    Checks for the required directories in the user's home folder.
+    If they do not exist they are created. The required directories are defined
+    by the variable dirs.
+    """
+    for each in dirs:
+        if not os.path.exists(each):
+            os.mkdir(each)
+
+def load_links():
+    """
+    Reads the .json file at the path defined by the variable f_links. If the file
+    exists the script will log an error but continue and process nothing. Perhaps
+    this should be more dramatic.
+    """
+    try:
+        with open(f_links, 'r') as f:
+            x = json.load(f)
+            links = []
+            for each in x:
+                links.append(Link(each))
+            log('FILE: \'{0}\' loaded'.format(f_links))
+            return links
+    except FileNotFoundError:
+        log('FILE: \'{0}\' does not exist'.format(f_links))
+        log('FILE: empty list being returned, no transfers will occur')
+        return []
+
+def log(s):
+    """
+    Logs the passed string variable s into logs defined by the variables f_log1
+    and f_log2. The length of these logs is capped by the variable max_log. If
+    the primary log (f_log1) becomes too large, the secondary log (f_log2) is deleted,
+    f_log1 becomes f_log2, and a new f_log1 is created.
+    """
+    max_log = 10000
+    timestamp = time.strftime('[%Y-%m-%d %H:%M:%S]', time.localtime())
+    
+    try:
+        if os.path.getsize(f_log1) > max_log:
+            os.remove(f_log2)
+            os.rename(f_log1, f_log2)
+
+            with open(f_log1, 'w') as f:
+                f.write(timestamp +
+                        'LOG SIZE EXCEEDED: \'{0}\' moved, \'{1}\' deleted'.format(f_log1,
+                                                                                   f_log2))
+                f.write('\n' + timestamp + s + '\n')
+        else:        
+            with open(f_log1, 'a') as f:
+                f.write(timestamp + s + '\n')
+    
+    except FileNotFoundError:
+        with open(f_log1, 'w') as f:
+            f.write(timestamp + s + '\n')
+
+def signal_SIGUSR1_handler(signum, frame):
+    """
+    WIP
+    """
+    pass
+
+def main():
+    """
+    Contains the script's main while loop that drives the entire daemon. For each link
+    described in the list links, the while loop determines if the appropriate interval
+    of time has passed before that link's command should be executed. If it has passed,
+    the while loop executes that command in a new thread.
+    """
+    log('INSTANCE: new instance of AutoClone started')
+
+    while(True):
+        hour = int(time.strftime('%H', time.localtime()))
+        minute = int(time.strftime('%M', time.localtime()))
+        minutes = minute + (60 * hour)
+
+        for each in links:
+            if minutes % each.interval == 0:
+                threading.Thread(target=each.transfer).start()
+        time.sleep(60)
+
+
+### PROGRAM START ###
+"""
+These variables define where all the files that AutoClone needs are located, besides
+the actual .py scripts. These variables should be changeable as you see fit. It will
+be necessary to edit them in all 3 .py scripts however (i.e. this script, the acsetup.py
+script, and the main autoclone script).
+"""
+dirs = [os.path.expanduser('~/.autoclone'),
+        os.path.expanduser('~/.autoclone/logs'),
+        os.path.expanduser('~/.autoclone/jobs')]
+f_links = dirs[0] + '/links.json'
+f_pid = dirs[0] + '/pid.dat'
+f_log1 = dirs[1] + '/daemon.log'
+f_log2 = dirs[1] + '/daemon_old.log'
+f_job_template = dirs[2] + '/job{}.dat'
+
+check_dirs()
+links = load_links()
+
+signal.signal(signal.SIGUSR1, signal_SIGUSR1_handler)
+
+if __name__ == '__main__':
+    main()
---
+++ acsetup.py
@@ -0,0 +1,293 @@
+#! /usr/bin/env python3
+"""
+This script provides a CLI to edit the .json file that stores all of the links
+used by AutoClone.
+
+Author: tgsachse (Tiger Sachse)
+Initial Release: 7/13/2017
+Current Release: 7/13/2017
+Version: 0.01
+License: GNU GPLv3
+"""
+import os
+import json
+import shutil
+from termcolor import cprint
+
+### CLASSES ###
+class Counter:
+    """
+    Returns a counter object.
+    Attributes: count
+    Functions: __init__, increment, reset
+    """
+    def __init__(self):
+        self.count = 0
+
+    def increment(self):
+        self.count += 1
+
+    def reset(self):
+        self.count = 0
+
+### FUNCTIONS ###
+# Generical functions #
+def header():
+    """
+    Clears the current window, then outputs the header defined by the variable
+    head. The header also includes the total number of unsaved changes made by the
+    user since the launch of the program.
+    """
+    os.system('clear')
+    text = "AutoClone Setup Utility (" + str(change_count.count) + " changes)"
+    head = '{:^{width}}'.format(text, width=window[0])
+    cprint(head + "\n", 'green')
+
+def input_restricted(prompt, valid):
+    """
+    Confirms that a given input is valid, as defined by the valid list var.
+    """
+    while(True):
+        i = input(prompt)
+        if i in valid:
+            return i
+        else:
+            print("Invalid input.")
+
+def print_restricted(s, space_list):
+    """
+    Prints to the window in 5 columns. Needed to list all links.
+    """
+    entry = ': {:^{w1}} : {:<{w2}} : {:<{w3}} : {:^{w4}} : {:^{w5}} :'
+    string = entry.format(s[0][:space_list[0]],
+                          s[1][:space_list[1]],
+                          s[2][:space_list[2]],
+                          s[3][:space_list[3]],
+                          s[4][:space_list[4]],
+                          w1=space_list[0],
+                          w2=space_list[1],
+                          w3=space_list[2],
+                          w4=space_list[3],
+                          w5=space_list[4])
+    print(string)
+
+def confirm_input():
+    """
+    Produces a prompt to allow the user to check his/her input.
+    """
+    print("Is this correct? (Y/n)")
+    valid = ['y', 'Y', 'yes', 'Yes', 'n', 'N', 'no', 'No']
+    
+    while(True):
+        i = input_restricted("> ", valid)
+        if i in valid[:4]:
+            return True
+        elif i in valid[4:]:
+            return False
+
+def load_links():
+    """
+    Loads the links saved in the f_links .json file. If the file does not
+    exist, the file is created and populated with an empty list. Finally
+    the results of the function are returned.
+    """
+    try:
+        with open(f_links, 'r') as f:
+            links = json.load(f)
+    except FileNotFoundError:
+        with open(f_links, 'w') as f:
+            empty_links = []
+            json.dump(empty_links, f)
+    finally:
+        with open(f_links, 'r') as f:
+            links = json.load(f)
+            return links
+
+def select_link():
+    """
+    Allows the user to select a link from a list of links.
+    """
+    while(True):
+        print("Enter the number of the value you'd like to modify.")
+        i = input("> ")
+        if i.isnumeric() and int(i) <= len(links) and int(i) > 0:
+            for link in links:
+                if link[0] == i:
+                    return link
+        else:
+            print("Invalid input. Please enter a number between 1 and " + str(len(links)) + ".")
+
+# Command functions #
+def command_help():
+    """
+    Prints a table of commands, names, and descriptions to the window.
+    """
+    header()
+    print("COMMAND : NAME   : DESCRIPTION\n" +
+          "      h : help   : Displays this help menu.\n" +
+          "      n : new    : Creates a new link.\n" +
+          "      r : remove : Removes a specific link.\n" +
+          "      e : edit   : Edits a specific link.\n" +
+          "      l : list   : Lists all existing links.\n" +
+          "      w : write  : Writes all changes to file.\n" +
+          "      c : clear  : Clears all changes.\n" +
+          "      q : quit   : Quits this setup utility.\n")
+
+def command_new():
+    """
+    Retrieves input from the user for a new link, which is then saved to the
+    links variable. This function, if completed properly, increments the
+    change_count.
+    """
+    header()
+    src = str(input("Source (absolute path): "))
+    dest = str(input("Destination (absolute path): "))
+    valid = ['sync', 'copy', 'move']
+    
+    while(True):
+        i = input("Interval (minutes): ")
+        if i.isnumeric() and int(i) > 0 and int(i) <= 1440:
+            intv = i
+            break
+        else:
+            print("Invalid input. Please enter a number between 1 and 1440.")
+
+    method = input_restricted("Method (sync/copy/move): ", valid)
+
+    if confirm_input():
+        change_count.increment()
+        num = str(len(links) + 1)
+        link = [num, src, dest, intv, method]
+        links.append(link)
+
+def command_remove():
+    """
+    Retrieves input from the user to remove an existing link. This function,
+    if completely properly, increments the change_count.
+    """
+    header()
+    command_list()
+
+    link = select_link()
+    change_count.increment()
+    links.remove(link)
+    for each in links:
+        each[0] = str(links.index(each) + 1)
+
+def command_edit():
+    """
+    Lists all links in the links variable. Then the user may select a specific
+    link to modify. After, the user selects which value of the link to modify.
+    Once properly completed, the function saves the edited values and link to the
+    links variable and increments the change_count.
+    """
+    header()
+    command_list()
+
+    valid = ['1', '2', '3', '4']
+    link = select_link()
+    header()
+    print("(1) Source:      " + link[1] + "\n" +
+          "(2) Destination: " + link[2] + "\n" +
+          "(3) Interval:    " + link[3] + "\n" +
+          "(4) Method:      " + link[4] + "\n")
+    
+    while(True):
+        print("Enter the number of the value you'd like to modify.")
+        i = input_restricted("> ", valid)
+        
+        print("Old value: " + str(links[int(link[0]) - 1][int(i)]))
+        new = input("New value: ")
+        if confirm_input():
+            change_count.increment()
+            links[int(link[0]) - 1][int(i)] = new
+            break
+
+def command_list():
+    """
+    Lists all of the links in the links variable, using the print_restricted
+    function.
+    """
+    example = ['#', 'SOURCE', 'DESTINATION', 'INTERVAL', 'METHOD']
+    print_restricted(example, spacing)
+    print("{:-^{w}}".format("-", w=window[0]))
+    for each in links:
+        print_restricted(each, spacing)
+    print("")
+
+def command_write():
+    """
+    Writes changes to file, and resets the change_count.
+    """
+    with open(f_links, 'w') as f:
+        json.dump(links, f)
+
+    print(str(change_count.count) + " change", end='')
+    if change_count.count != 1:
+        print("s", end='')
+    print(" saved to file.")
+    change_count.reset()
+
+
+### BEGIN PROGRAM ###
+"""
+The f_links variable can be changed to save the .json file anywhere, under any
+name. If this is done the f_links variable must be changed in the other two scripts,
+however. Those two scripts are the main autoclone script and the acdaemon.py script.
+"""
+f_links = "links.json"
+links = load_links()
+change_count = Counter()
+valid_commands = ['h', 'n', 'r', 'e', 'l', 'w', 'c', 'q']
+
+"""
+The following variables determine the size of the current terminal window and then
+save the appropriate spacings into a spacing list. This list is used by the
+command_list() function.
+"""
+window = shutil.get_terminal_size()
+spacing = [3, None, None, 8, 6]
+f_name_spacing = int((window[0] - spacing[0] - spacing[3] - spacing[4] - 16)/2)
+spacing[1] = f_name_spacing
+spacing[2] = f_name_spacing #CLEANUP######################################
+
+header()
+while(True):
+    print("Enter a command. ('h' for help)")
+    command = input_restricted("> ", valid_commands)
+
+    if command == 'h':
+        command_help()
+
+    elif command == 'n':
+        command_new()
+        header()
+
+    elif command == 'r':
+        command_remove()
+        header()
+
+    elif command == 'e':
+        command_edit()
+        header()
+
+    elif command == 'l':
+        header()
+        command_list()
+
+    elif command == 'w':
+        command_write()
+
+    elif command == 'c':
+        change_count.reset()
+        links = load_links()
+        header()
+
+    elif command == 'q':
+        header()
+        print("You would like to quit.")
+        if confirm_input():
+            os.system('clear')
+            quit()
+        else:
+            header()

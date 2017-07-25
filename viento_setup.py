46d7729788a33c0ddd0df0ab7605eac6513bc188
Tiger Sachse <tiger@sachse.io>
2017-07-25 20:04:51 +0000

    rewrote daemon, renamed links to drafts

--- viento_setup.py
+++ viento_setup.py
@@ -32,86 +32,36 @@ class Counter:
         self.count = 0
 
 ### FUNCTIONS ###
-def header():
+def main():
     """
-    Clears the current window, then outputs the header defined by the variable
-    head. The header also includes the total number of unsaved changes made by the
-    user since the last write to file.
+    Defines a valid dictionary that contains all possible commands. When the user
+    inputs a command, it is found in the dictionary and executed.
     """
-    os.system('clear')
-    text = "Viento Setup Utility (" + str(change_count.count) + " changes)"
-    head = '{:^{width}}'.format(text, width=window[0])
-    cprint(head + "\n", 'green')
-
-def select_link():
-    """
-    Allows the user to select a link from a list of links.
-    """
+    valid = {'c':command_clear,
+             'e':command_edit,
+             'h':command_help,
+             'l':command_list,
+             'n':command_new,
+             'q':command_quit,
+             'r':command_remove,
+             'w':command_write}
+    header()
     while(True):
-        print("Enter the number of the value you'd like to modify.")
-        i = input("> ")
-        if i.isnumeric() and int(i) <= len(links) and int(i) > 0:
-            for link in links:
-                if link[0] == i:
-                    return link
-        else:
-            print("Invalid input. Please enter a number between 1 and " + str(len(links)) + ".")
+        print("Enter a command. ('h' for help)")
+        command = viento_utils.input_restricted("> ", valid)
+        for each in valid:
+            if command == each:
+                valid[each]()
+                break
 
-def command_help():
+def command_clear():
     """
-    Prints a table of commands, names, and descriptions to the window.
+    Clears all changes since the last file read by reconstructing the links
+    variable via a new file read (load_links()). This resets the change_count.
     """
+    change_count.reset()
+    links = viento_utils.drafts_load()#####probably has to be global
     header()
-    print("COMMAND : NAME   : DESCRIPTION\n" +
-          "      h : help   : Displays this help menu.\n" +
-          "      n : new    : Creates a new link.\n" +
-          "      r : remove : Removes a specific link.\n" +
-          "      e : edit   : Edits a specific link.\n" +
-          "      l : list   : Lists all existing links.\n" +
-          "      w : write  : Writes all changes to file.\n" +
-          "      c : clear  : Clears all changes.\n" +
-          "      q : quit   : Quits this setup utility.\n")
-
-def command_new():
-    """
-    Retrieves input from the user for a new link, which is then saved to the
-    links variable. This function, if completed properly, increments the
-    change_count.
-    """
-    header()
-    src = str(input("Source (absolute path): "))
-    dest = str(input("Destination (absolute path): "))
-    valid = ['sync', 'copy', 'move']
-    
-    while(True):
-        i = input("Interval (minutes): ")
-        if i.isnumeric() and int(i) > 0 and int(i) < 1439:
-            intv = i
-            break
-        else:
-            print("Invalid input. Please enter a number between 1 and 1439.")
-
-    method = viento_utils.rinput("Method (sync/copy/move): ", valid)
-
-    if viento_utils.confirm_input():
-        change_count.increment()
-        num = str(len(links) + 1)
-        link = [num, src, dest, intv, method]
-        links.append(link)
-
-def command_remove():
-    """
-    Retrieves input from the user to remove an existing link. This function,
-    if completely properly, increments the change_count.
-    """
-    header()
-    command_list()
-
-    link = select_link()
-    change_count.increment()
-    links.remove(link)
-    for each in links:
-        each[0] = str(links.index(each) + 1)
 
 def command_edit():
     """
@@ -124,23 +74,37 @@ def command_edit():
     command_list()
 
     valid = ['1', '2', '3', '4']
-    link = select_link()
+    draft = draft_select()
     header()
-    print("(1) Source:      " + link[1] + "\n" +
-          "(2) Destination: " + link[2] + "\n" +
-          "(3) Interval:    " + link[3] + "\n" +
-          "(4) Method:      " + link[4] + "\n")
+    print("(1) Source:      " + draft[1] + "\n" +
+          "(2) Destination: " + draft[2] + "\n" +
+          "(3) Interval:    " + draft[3] + "\n" +
+          "(4) Method:      " + draft[4] + "\n")
     
     while(True):
         print("Enter the number of the value you'd like to modify.")
-        i = viento_utils.rinput("> ", valid)
+        i = viento_utils.input_restricted("> ", valid)
         
-        print("Old value: " + str(links[int(link[0]) - 1][int(i)]))
+        print("Old value: " + str(drafts[int(draft[0]) - 1][int(i)]))
         new = input("New value: ")
         if viento_utils.confirm_input():
             change_count.increment()
-            links[int(link[0]) - 1][int(i)] = new
-            break
+            drafts[int(draft[0]) - 1][int(i)] = new
+
+def command_help():
+    """
+    Prints a table of commands, names, and descriptions to the window.
+    """
+    header()
+    print("COMMAND : NAME   : DESCRIPTION\n" +
+          "      h : help   : Displays this help menu.\n" +
+          "      n : new    : Creates a new draft.\n" +
+          "      r : remove : Removes a specific draft.\n" +
+          "      e : edit   : Edits a specific draft.\n" +
+          "      l : list   : Lists all existing drafts.\n" +
+          "      w : write  : Writes all changes to file.\n" +
+          "      c : clear  : Clears all changes.\n" +
+          "      q : quit   : Quits this setup utility.\n")
 
 def command_list():
     """
@@ -156,35 +120,39 @@ def command_list():
                 ': {:<{width}} ',
                 ': {:^{width}} ',
                 ': {:^{width}} :']
-    viento_utils.rprint(example, template, spacing, color='cyan')
+    viento_utils.print_restricted(example, template, spacing, color='cyan')
     print("")
     print("{:-^{w}}".format("-", w=window[0]))
-    for each in links:
-        viento_utils.rprint(each, template, spacing, color='cyan')
+    for each in drafts:
+        viento_utils.print_restricted(each, template, spacing, color='cyan')
     print("\n")
 
-def command_write():
+def command_new():
     """
-    Writes changes to file, and resets the change_count.
+    Retrieves input from the user for a new link, which is then saved to the
+    links variable. This function, if completed properly, increments the
+    change_count.
     """
-    viento_utils.check_directories()
-    with open(viento_utils.f_links, 'w') as f:
-        json.dump(links, f)
-
-    print(str(change_count.count) + " change", end='')
-    if change_count.count != 1:
-        print("s", end='')
-    print(" saved to file.")
-    change_count.reset()
-
-def command_clear():
-    """
-    Clears all changes since the last file read by reconstructing the links
-    variable via a new file read (load_links()). This resets the change_count.
-    """
-    change_count.reset()
-    links = viento_utils.load_links()#####probably has to be global
     header()
+    source = str(input("Source (absolute path): "))
+    destination = str(input("Destination (absolute path): "))
+    valid = ['sync', 'copy', 'move']
+    
+    while(True):
+        i = input("Interval (minutes): ")
+        if i.isnumeric() and int(i) > 0 and int(i) < 1439:
+            interval = i
+            break
+        else:
+            print("Invalid input. Please enter a number between 1 and 1439.")
+
+    method = viento_utils.input_restricted("Method (sync/copy/move): ", valid)
+
+    if viento_utils.confirm_input():
+        change_count.increment()
+        num = str(len(drafts) + 1)
+        draft = [num, source, destination, interval, method]
+        drafts.append(draft)
 
 def command_quit():
     """
@@ -197,31 +165,62 @@ def command_quit():
     else:
         header()
 
-def main():
+def command_remove():
     """
-    Defines a valid dictionary that contains all possible commands. When the user
-    inputs a command, it is found in the dictionary and executed.
+    Retrieves input from the user to remove an existing link. This function,
+    if completely properly, increments the change_count.
     """
-    valid = {'h':command_help,
-             'n':command_new,
-             'r':command_remove,
-             'e':command_edit,
-             'l':command_list,
-             'w':command_write,
-             'c':command_clear,
-             'q':command_quit}
     header()
+    command_list()
+
+    draft = draft_select()
+    change_count.increment()
+    drafts.remove(draft)
+    for each in drafts:
+        each[0] = str(drafts.index(each) + 1)
+
+def command_write():
+    """
+    Writes changes to file, and resets the change_count.
+    """
+    viento_utils.directories_check()
+    with open(viento_utils.f_drafts, 'w') as f:
+        json.dump(drafts, f)
+
+    print(str(change_count.count) + " change", end='')
+    if change_count.count != 1:
+        print("s", end='')
+    print(" saved to file.")
+    change_count.reset()
+
+def draft_select():
+    """
+    Allows the user to select a link from a list of links.
+    """
     while(True):
-        print("Enter a command. ('h' for help)")
-        command = viento_utils.rinput("> ", valid)
-        for each in valid:
-            if command == each:
-                valid[each]()
-                break
+        print("Enter the number of the value you'd like to modify.")
+        i = input("> ")
+        if i.isnumeric() and int(i) <= len(drafts) and int(i) > 0:
+            for draft in drafts:
+                if draft[0] == i:
+                    return draft
+        else:
+            print("Invalid input. Please enter a number between 1 and " + str(len(drafts)) + ".")
+
+def header():
+    """
+    Clears the current window, then outputs the header defined by the variable
+    head. The header also includes the total number of unsaved changes made by the
+    user since the last write to file.
+    """
+    os.system('clear')
+    text = "Viento Setup Utility (" + str(change_count.count) + " changes)"
+    head = '{:^{width}}'.format(text, width=window[0])
+    cprint(head + "\n", 'green')
 
 ### BEGIN PROGRAM ###
-viento_utils.check_directories()
-links = viento_utils.load_links()
+viento_utils.directories_check()
+drafts = viento_utils.drafts_load()
 change_count = Counter()
 window = shutil.get_terminal_size()
 
--- viento_utils.py
+++ viento_utils.py
@@ -16,16 +16,6 @@ import time
 import json
 from termcolor import cprint
 
-def check_directories():
-    """
-    Checks for the necessary directories in the user's home folder. If they do not exist
-    they are created. The necessary directories are defined by the directories variable.
-    """
-    for each in directories:
-        if not os.path.exists(each):
-            #log('FILE: {0} did not exist so it was created'.format(each))
-            os.mkdir(each)
-
 def confirm_input(prompt="> ", ask="Is this correct? (Y/n)"):
     """
     Prompts the user to validate his/her input.
@@ -34,30 +24,51 @@ def confirm_input(prompt="> ", ask="Is this correct? (Y/n)"):
     valid = ['y', 'Y', 'yes', 'Yes', 'n', 'N', 'no', 'No']
     
     while(True):
-        i = rinput(prompt, valid)
+        i = input_restricted(prompt, valid)
         if i in valid[:4]:
             return True
         elif i in valid[4:]:
             return False
 
-def load_links():
+def directories_check():
+    """
+    Checks for the necessary directories in the user's home folder. If they do not exist
+    they are created. The necessary directories are defined by the directories variable.
+    """
+    for each in directories:
+        if not os.path.exists(each):
+            os.mkdir(each)
+
+def drafts_load():
     """
     Reads the links file defined by the variable f_links. If the file exists it is loaded
     into the program. If it does not, an empty list is loaded and the error is logged.
     """
-    check_directories()
+    directories_check()
     try:
-        with open(f_links, 'r') as f:
-            links = json.load(f)
-            log('FILE: \'{0}\' loaded'.format(f_links))
-            return links
+        with open(f_drafts, 'r') as f:
+            drafts = json.load(f)
+            log('FILE: \'{0}\' loaded'.format(f_drafts))
     except FileNotFoundError:
-        with open(f_links, 'w') as f:
-            links = []
-            json.dump(links, f)
-            log('FILE: \'{0}\' does not exist'.format(f_links))
+        with open(f_drafts, 'w') as f:
+            drafts = []
+            json.dump(drafts, f)
+            log('FILE: \'{0}\' does not exist'.format(f_drafts))
             log('FILE: empty list being returned, no transfer will occur')
-            return links
+    return drafts
+
+def input_restricted(prompt, valid, invalid="Invalid input."):
+    """
+    Compares user input to a list of valid responses. If the input is in the valid
+    list, the input is returned, else the invalid statement is printed and the user
+    is allowed to enter input again.
+    """
+    while(True):
+        i = input(prompt)
+        if i in valid:
+            return i
+        else:
+            print(invalid)
 
 def log(s, leading_newline=False):
     """
@@ -65,17 +76,16 @@ def log(s, leading_newline=False):
     log exceeds a certain size (MAX_LOG) then the log is shelved and a new log is created.
     After 5 total logs exist the oldest log will be deleted before each shelving action.
     """
-    MAX_LOG = 10000
     timestamp = time.strftime('[%Y-%m-%d %H:%M:%S]', time.localtime())
-    check_directories()
+    directories_check()
     try:
         if os.path.getsize(f_log.format(1)) > MAX_LOG:
             try:
-                os.remove(f_log.format(5))
+                os.remove(f_log.format(MAX_LOGS))
             except OSError:
                 pass
 
-            for i in reversed(range(2,6)):
+            for i in reversed(range(2,MAX_LOGS-1)):
                 try:
                     os.rename(f_log.format(i-1), f_log.format(i))
                 except OSError:
@@ -99,20 +109,7 @@ def log(s, leading_newline=False):
                 f.write('\n')
             f.write(timestamp + s + '\n')
 
-def rinput(prompt, valid, invalid="Invalid input."):
-    """
-    Compares user input to a list of valid responses. If the input is in the valid
-    list, the input is returned, else the invalid statement is printed and the user
-    is allowed to enter input again.
-    """
-    while(True):
-        i = input(prompt)
-        if i in valid:
-            return i
-        else:
-            print(invalid)
-
-def rprint(strings, template, spacing, color=None):
+def print_restricted(strings, template, spacing, color=None):
     """
     Prints a list of strings spaced out on the screen according to an accompanying
     template list. Output can be colored via cprint and the color variable.
@@ -128,6 +125,47 @@ def rprint(strings, template, spacing, color=None):
         else:
             cprint(each, color, end='')
 
+def statistics_format():#UNFINISHED
+    """
+    """
+    stats = statistics_open()
+    fstats = []
+    for k,v in stats.items():
+        fstats.append([k,v])
+    print(fstats)
+
+def statistics_open():#UNFINISHED
+    """
+    """
+    try:
+        with open(f_stats, 'r') as f:
+            stats = json.load(f)
+    except FileNotFoundError:
+        stats = statistics
+    return stats
+
+def statistics_print():#UNFINISHED
+    """
+    """
+    stats = statistics_format()
+    template = ['{:<{width}}','{:^{width}}']
+    spacing = [20,25]
+    '''
+    for k,v in statistics.items():
+        rprint([k,str(v)], template, spacing)
+        print("")
+    '''
+def statistics_record(stat, value):#UNFINISHED
+    """
+    """
+    stats = statistics_open()
+    for key in stats:
+        if stat == key:
+            stats[key] += value
+
+    with open(f_stats, 'w') as f:
+        json.dump(stats, f)
+
 """
 The following variables are paths to various files and directories required for this
 program to function properly. By design, these directories and files can be renamed here
@@ -141,7 +179,29 @@ directories = [os.path.expanduser('~/.config'),
                os.path.expanduser('~/.viento'),
                os.path.expanduser('~/.viento/logs'),
                os.path.expanduser('~/.viento/jobs')]
-f_links = directories[3] + '/links.json'
 f_service = directories[2] + '/viento.service'
+f_drafts = directories[3] + '/drafts.json'
+f_stats = directories[4] + '/stats.log'
 f_log = directories[4] + '/log{}.log'
 f_job = directories[5] + '/job{}.dat'
+
+MAX_LOG = 10000
+MAX_LOGS = 5
+
+statistics = {'transfers' : ["Total Transfers:", 0],
+              'uptime_min' : ["Uptime (minutes):", 0],
+              'uptime_days' : ["Uptime (days/hours/minutes):", 0],
+              'bytes' : ["Bytes transferred:", 0],
+              'gigabytes' : ["Gigabytes transferred:", 0]}
+
+log_entries = {'inst_new' : 'INSTANCE: new instance of Viento started',
+               'state_enter' : 'STATE: entering heightened state for \'{0}\' {1}',
+               'state_change' : 'STATE: {0} interval changed to every {1} minute(s)',
+               'state_leave' : 'STATE: leaving heightened state for \'{0}\' {1}',
+               'state_revert' : 'STATE: {0} interval reverted to every {1} minute(s)',
+               'trans_success' : 'TRANSFER: {0} \'{1}\' >> \'{2}\'',
+               'file_load' : 'FILE: \'{0}\' loaded',
+               'file_dne' : 'FILE: \'{0}\' does not exist',
+               'file_empty' : 'FILE: empty list being returned, no transfer will occur',
+               'file_size' : 'FILE: log size exceeded, logs shifted'}
+               

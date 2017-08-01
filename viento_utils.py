e453e4a0f0ab8b91cc688b567da3c4c7f42baf8a
Tiger Sachse <tgsachse@gmail.com>
2017-08-01 16:10:17 +0000

    final build for 0.5.0-beta

--- viento_utils.py
+++ viento_utils.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """
-Common utility functions for viento. This script includes functions used by other aspects
-of the viento program, as well as paths for all non-.py files used by the program. If you 
-would like to change the paths for any of the non-.py files feel free to edit them below.
+Common utility functions for viento. This script includes functions used by
+other aspects of the viento program, as well as paths for all non-.py files
+used by the program. If you would like to change the paths for any of the
+non-.py files feel free to edit them below.
 
 Author: tgsachse (Tiger Sachse)
 Initial Release: 7/13/2017
-Current Release: 7/27/2017
+Current Release: 8/01/2017
 Version: 0.5.0-beta
 License: GNU GPLv3
 """
@@ -31,8 +32,9 @@ def confirm_input(prompt="> ", ask="Is this correct? (Y/n)"):
 
 def directories_check():
     """
-    Checks for the necessary directories in the user's home folder. If they do not exist
-    they are created. The necessary directories are defined by the directories list.
+    Checks for the necessary directories in the user's home folder. If they do
+    not exist they are created. The necessary directories are defined by the
+    directories list.
     """
     for each in directories:
         if not os.path.exists(each):
@@ -40,8 +42,9 @@ def directories_check():
 
 def drafts_load():
     """
-    Reads the drafts file defined by the variable f_drafts. If the file exists it is loaded
-    into the program. If it does not, an empty list is loaded and the error is logged.
+    Reads the drafts file defined by the variable f_drafts. If the file exists
+    it is loaded into the program. If it does not, an empty list is loaded and
+    the error is logged.
     """
     directories_check()
     try:
@@ -58,9 +61,9 @@ def drafts_load():
 
 def input_restricted(prompt, valid, invalid="Invalid input."):
     """
-    Compares user input to a list of valid responses. If the input is in the valid
-    list, the input is returned, else the invalid statement is printed and the user
-    is allowed to enter input again.
+    Compares user input to a list of valid responses. If the input is in the
+    valid list, the input is returned, else the invalid statement is printed
+    and the user is allowed to enter input again.
     """
     while(True):
         i = input(prompt)
@@ -71,8 +74,9 @@ def input_restricted(prompt, valid, invalid="Invalid input."):
 
 def log(key, args=[], leading_newline=False):
     """
-    Writes a formatted string (key formatted with args) to the log file (f_log). If the
-    primary log file becomes too large, the logs are shifted using shift_logs().
+    Writes a formatted string (key formatted with args) to the log
+    file (f_log). If the primary log file becomes too large, the logs are
+    shifted using shift_logs().
     """
     directories_check()###
     timestamp = time.strftime('[%Y-%m-%d %H:%M:%S] ', time.localtime())
@@ -93,8 +97,9 @@ def log(key, args=[], leading_newline=False):
 
 def print_restricted(strings, template, spacing, color=None):
     """
-    Prints a list of strings spaced out on the screen according to an accompanying
-    template list. Output can be colored via cprint and the color variable.
+    Prints a list of strings spaced out on the screen according to an
+    accompanying template list. Output can be colored via cprint and the color
+    variable.
     """
     string_list = []
     for each in zip(strings, template, spacing):
@@ -109,10 +114,11 @@ def print_restricted(strings, template, spacing, color=None):
 
 def setup_check():
     """
-    Checks for the existence of critical files and directories. If no drafts file
-    (defined in viento_utils) exists then the program exits. If no service file
-    (also defined in viento_utils) exists then the user is prompted to generate one.
-    The template for this generation is defined by the list t_service.
+    Checks for the existence of critical files and directories. If no drafts
+    file (defined in viento_utils) exists then the program exits. If no service
+    file (also defined in viento_utils) exists then the user is prompted to
+    generate one. The template for this generation is defined by the
+    list t_service.
     """
     if not os.path.exists(f_drafts):
         print("No drafts file exists. Please run 'viento setup' to make one.")
@@ -120,7 +126,8 @@ def setup_check():
 
     if not os.path.exists(f_service):
         directories_check()
-        s = "No service file exists. Would you like to create one at {}?".format(f_service)
+        s = ("No service file exists." + 
+             "Would you like to create one at {}?".format(f_service))
         if confirm_input(prompt="(Y/n) > ", ask=s):
             with open(f_service, 'w') as f:
                 for each in t_service:
@@ -131,30 +138,21 @@ def setup_check():
 
 def shift_logs():
     """
-    If the primary log exceeds a certain size (MAX_LOG) then the log is shelved and a
-    new log is created. The maximum number of log files is defined by MAX_LOGS. After
-    the max number of logs have been produced the oldest log will be deleted before each
-    shelving action.
+    If the primary log exceeds a certain size (MAX_LOG) then the log is shelved
+    and a new log is created. The maximum number of log files is defined by
+    MAX_LOGS. After the max number of logs have been produced the oldest log
+    will be deleted before each shelving action.
     """
     try:
         os.remove(f_log.format(MAX_LOGS))
     except OSError:
         pass
     
-    for x in reversed(range(2, MAX_LOGS-1)):
+    for x in reversed(range(2, MAX_LOGS+1)):
         try:
             os.rename(f_log.format(x-1), f_log.format(x))
         except OSError:
             pass
-
-def statistics_format():#UNFINISHED
-    """
-    """
-    stats = statistics_open()
-    fstats = []
-    for k,v in stats.items():
-        fstats.append([k,v])
-    print(fstats)
 
 def statistics_open():
     """
@@ -168,17 +166,19 @@ def statistics_open():
         stats = t_statistics
     return stats
 
-def statistics_print():#UNFINISHED
+def statistics_print():
     """
+    Prints statistics onto the screen using print_restricted(). Called in the
+    main viento script.
     """
-    stats = statistics_format()
-    template = ['{:<{width}}','{:^{width}}']
-    spacing = [20,25]
-    '''
-    for k,v in statistics.items():
-        rprint([k,str(v)], template, spacing)
+    stats = statistics_open()
+    template = ['{:>{width}}',' {:<{width}}']
+    spacing = [30,40]
+    
+    for k,v in stats.items():
+        print_restricted([v[0],str(v[1])], template, spacing)
         print("")
-    '''
+    
 def statistics_record(stat, value):
     """
     Increments the appropriate stat by a defined value, and then saves to file.
@@ -186,17 +186,31 @@ def statistics_record(stat, value):
     stats = statistics_open()
     for key in stats:
         if stat == key:
-            stats[key] += value
+            stats[key][1] += int(value)
+
+    if stat == 'bytes':
+        stats['d_gigabytes'][1] = float(stats['bytes'][1]/1024/1024/1024)
+
+    elif stat == 'uptime_min':
+        if stats['d_uptime_days'][1][2] < 59:
+            stats['d_uptime_days'][1][2] += 1
+        else:
+            stats['d_uptime_days'][1][2] = 0
+            if stats['d_uptime_days'][1][1] < 23:
+                stats['d_uptime_days'][1][1] += 1
+            else:
+                stats['d_uptime_days'][1][1] = 0
+                stats['d_uptime_days'][1][0] += 1
 
     with open(f_stats, 'w') as f:
         json.dump(stats, f)
 
 """
-The following variables are paths to various files and directories required for this
-program to function properly. By design, these directories and files can be renamed here
-and the rest of the program will follow suit. If you feel the need to change the paths
-for the program's files, please change them here and not elsewhere, otherwise you'll stumble
-into heaps of FileNotFoundErrors. :)
+The following variables are paths to various files and directories required for
+this program to function properly. By design, these directories and files can
+be renamed here and the rest of the program will follow suit. If you feel the
+need to change the paths for the program's files, please change them here and
+not elsewhere, otherwise you'll stumble into heaps of FileNotFoundErrors. :)
 """
 directories = [os.path.expanduser('~/.config'),
                os.path.expanduser('~/.config/systemd'),
@@ -223,23 +237,24 @@ t_service = ['[Unit]\n',
              'WantedBy=default.target']
 
 t_statistics = {'transfers' : ["Total Transfers:", 0],
-                'uptime_min' : ["Uptime (minutes):", 0],
-                'uptime_days' : ["Uptime (days/hours/minutes):", 0],
                 'bytes' : ["Bytes transferred:", 0],
-                'gigabytes' : ["Gigabytes transferred:", 0]}
+                'uptime_min' : ["Uptime (minutes):", 0],
+                'd_uptime_days' : ["Uptime (days/hours/minutes):", [0,0,0]],
+                'd_gigabytes' : ["Gigabytes transferred:", 0.000]}
 
-log_entries = {'inst_new' : 'INSTANCE: new instance of Viento started',
-               'inst_start' : 'INSTANCE: Viento start command executed',
-               'inst_stop' : 'INSTANCE: Viento stop command executed',
-               'inst_enable' : 'INSTANCE: Viento enabled at boot',
-               'inst_disable' : 'INSTANCE: Viento disabled at boot',
-               'state_enter' : 'STATE: entering heightened state for \'{0[0]}\' {0[1]}',
-               'state_change' : 'STATE: {0[0]} interval changed to every {0[1]} minute(s)',
-               'state_leave' : 'STATE: leaving heightened state for \'{0[0]}\' {0[1]}',
-               'state_revert' : 'STATE: {0[0]} interval reverted to every {0[1]} minute(s)',
-               'trans_success' : 'TRANSFER: {0[0]} \'{0[1]}\' >> \'{0[2]}\'',
-               'trans_force' : 'TRANSFER: {0[0]} \'{0[1]}\' >> \'{0[2]}\' (forced)',
-               'file_write' : 'FILE: {0[0]} change(s) made to \'{0[1]}\'',
-               'file_load' : 'FILE: \'{0[0]}\' loaded',
-               'file_dne' : 'FILE: \'{0[0]}\' does not exist',
-               'file_empty' : 'FILE: empty list being returned, no transfer will occur'}
+log_entries = {
+    'inst_new':'INSTANCE: new instance of Viento started',
+    'inst_start':'INSTANCE: Viento start command executed',
+    'inst_stop':'INSTANCE: Viento stop command executed',
+    'inst_enable':'INSTANCE: Viento enabled at boot',
+    'inst_disable':'INSTANCE: Viento disabled at boot',
+    'state_enter':'STATE: entering heightened state for \'{0[0]}\' {0[1]}',
+    'state_change':'STATE: {0[0]} interval changed to every {0[1]} minute(s)',
+    'state_leave':'STATE: leaving heightened state for \'{0[0]}\' {0[1]}',
+    'state_revert':'STATE: {0[0]} interval reverted to every {0[1]} minute(s)',
+    'trans_success':'TRANSFER: {0[0]} \'{0[1]}\' >> \'{0[2]}\'',
+    'trans_force':'TRANSFER: {0[0]} \'{0[1]}\' >> \'{0[2]}\' (forced)',
+    'file_write':'FILE: {0[0]} change(s) made to \'{0[1]}\'',
+    'file_load':'FILE: \'{0[0]}\' loaded',
+    'file_dne':'FILE: \'{0[0]}\' does not exist',
+    'file_empty':'FILE: empty list being returned, no transfer will occur'}

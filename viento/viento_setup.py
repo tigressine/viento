c30f33a4ee9f23291d3467fc2b67f5324ae87245
Tiger Sachse <tiger@sachse.io>
2017-08-31 09:27:25 -0400

    Update viento_setup.py

--- viento/viento_setup.py
+++ viento/viento_setup.py
@@ -5,8 +5,8 @@ used by viento.
 
 Author: tgsachse (Tiger Sachse)
 Initial Release: 7/13/2017
-Current Release: 8/01/2017
-Version: 0.5.0-beta
+Current Release: 8/31/2017
+Version: 0.6.0-beta
 License: GNU GPLv3
 """
 import os
